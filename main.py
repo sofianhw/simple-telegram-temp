117f8d61ea42f65a0438dfbc5def81d16d48b893
sofian hadiwijaya <me.sofian.hadiwijaya@gmail.com>
2024-01-24 15:58:24 +0700

    add error handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import telebot, os
+import telebot, os, time
 from bot_handlers import setup_handlers
 
 # Initialize the bot with your token
@@ -8,5 +8,11 @@ bot = telebot.TeleBot(os.environ.get('BOT_ID'))
 setup_handlers(bot)
 
 # Start polling
-bot.infinity_polling(interval=0, timeout=20)
+while True:
+    try:
+        bot.infinity_polling(interval=0.1, timeout=10)
+    except Exception as e:
+        # Log the exception here
+        print(f"Error encountered: {e}. Restarting polling...")
+        time.sleep(1)
 
